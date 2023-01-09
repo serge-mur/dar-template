d115ed739dbdfa9447f91e3af8316b58f606f17f
serge <serge-mur@yandex.ru>
2023-01-09 12:24:24 +0300

    verstka

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -11,4 +11,43 @@ document.addEventListener('DOMContentLoaded', function(event) {
     mediaQueryMobile.addListener(mediaChange)
     mediaChange(mediaQueryMobile)
 
+
+    let swiperVideo = new Swiper(".swiper-video", {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 30,
+        autoHeight: true,
+        grabCursor: true,
+        breakpoints: {
+            992: {
+                slidesPerView: 3,
+                slidesPerGroup: 1,
+            },
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+    });
+
+    const videoModal = document.getElementById('videoModal')
+    videoModal.addEventListener('show.bs.modal', event => {
+        const button = event.relatedTarget
+        const video = button.getAttribute('data-bs-video')
+        const modalBody = videoModal.querySelector('.modal-body')
+        modalBody.innerHTML = `<div class="ratio ratio-16x9"><iframe src="https://www.youtube.com/embed/${video}?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>`
+    })
+    videoModal.addEventListener('hidden.bs.modal', event => {
+        const modalBody = videoModal.querySelector('.modal-body')
+        modalBody.innerHTML = ''
+    })
+
+
+
 })
+
+
